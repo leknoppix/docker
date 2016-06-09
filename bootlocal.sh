3c9a4d1362b47a8b870b6ba5060fbad352a30411
Pascal Cañadas <leknoppix@users.noreply.github.com>
2016-06-09 17:40:39 +0200

    Amélioration des logs nginx

--- bootlocal.sh
+++ bootlocal.sh
@@ -1,10 +1,10 @@
-TERM=xterm-color
-tce-load -wi nano
 mkdir /var/www
 mkdir /var/mysql
 mkdir /var/configuration
-mkdir /var/www/logs_nginx
+mkdir /var/log/nginx/
 mount -t vboxsf www /var/www
 mount -t vboxsf mysql /var/mysql
 mount -t vboxsf config /var/configuration
-mount -t vboxsf logs /var/www/logs_nginx
+mount -t vboxsf logs /var/log/nginx/
+TERM=xterm-color
+tce-load -wi nano
