64e66a5171b9898d72a9b95409acdd65b1e3dfbc
Pascal Cañadas <leknoppix@users.noreply.github.com>
2016-06-17 14:30:53 +0100

    Docker Clé 3g

--- bootlocal.sh
+++ bootlocal.sh
@@ -2,9 +2,11 @@ mkdir /var/www
 mkdir /var/mysql
 mkdir /var/configuration
 mkdir /var/log/nginx/
+mkdir /dropbox/
 mount -t vboxsf www /var/www
 mount -t vboxsf mysql /var/mysql
 mount -t vboxsf config /var/configuration
 mount -t vboxsf logs /var/log/nginx/
+mount -t vboxsf gitdropbox /dropbox/
 TERM=xterm-color
 tce-load -wi nano
