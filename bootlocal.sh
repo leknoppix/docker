7326d517b250c31c679d29e033b88918f4a96ce7
Pascal Cañadas <leknoppix@users.noreply.github.com>
2016-09-06 21:49:34 +0200

    Correction d'organisation des logs

--- bootlocal.sh
+++ bootlocal.sh
@@ -2,13 +2,13 @@ mkdir /var/www
 mkdir /var/ruby
 mkdir /var/mysql
 mkdir /var/configuration
-mkdir /var/log
+mkdir /var/log/nginx
 mkdir /dropbox/
 mount -t vboxsf www /var/www
 mount -t vboxsf ruby /var/ruby
 mount -t vboxsf mysql /var/mysql
 mount -t vboxsf config /var/configuration
-mount -t vboxsf logs /var/log/
-mount -t vboxsf gitdropbox /dropbox/
+mount -t vboxsf logs /var/log/nginx
+mount -t vboxsf gitdropbox /dropbox
 TERM=xterm-color
 tce-load -wi nano
