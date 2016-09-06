8ba05be5d96cbb7c51715e93e0739438605ffc2c
Pascal Cañadas <leknoppix@users.noreply.github.com>
2016-09-06 23:09:49 +0200

    Ajout de l'élément de nodejs

--- bootlocal.sh
+++ bootlocal.sh
@@ -1,12 +1,14 @@
 mkdir /var/www
+mkdir /var/nodejs
 mkdir /var/ruby
 mkdir /var/mysql
 mkdir /var/configuration
 mkdir /var/log/nginx
-mkdir /dropbox/
+mkdir /dropbox
 mount -t vboxsf www /var/www
 mount -t vboxsf ruby /var/ruby
 mount -t vboxsf mysql /var/mysql
+mount -t vboxsf nodejs /var/nodejs
 mount -t vboxsf config /var/configuration
 mount -t vboxsf logs /var/log/nginx
 mount -t vboxsf gitdropbox /dropbox
