96cfe0b2819b06e4a3c8f81da261ea5124a91226
Pascal Cañadas <leknoppix@users.noreply.github.com>
2016-09-06 18:48:32 +0200

    Modification diverses

--- bootlocal.sh
+++ bootlocal.sh
@@ -2,13 +2,13 @@ mkdir /var/www
 mkdir /var/ruby
 mkdir /var/mysql
 mkdir /var/configuration
-mkdir /var/log/nginx/
+mkdir /var/log
 mkdir /dropbox/
 mount -t vboxsf www /var/www
 mount -t vboxsf ruby /var/ruby
 mount -t vboxsf mysql /var/mysql
 mount -t vboxsf config /var/configuration
-mount -t vboxsf logs /var/log/nginx/
+mount -t vboxsf logs /var/log/
 mount -t vboxsf gitdropbox /dropbox/
 TERM=xterm-color
 tce-load -wi nano
